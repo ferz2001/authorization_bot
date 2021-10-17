0f03251bb18441535c468849ab48f0476b2d4d3c
ferz2001 <86674314+ferz2001@users.noreply.github.com>
2021-10-17 14:44:19 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,32 +1,71 @@
-from selenium import webdriver
+import os
+import requests
 import time
+import ast
+import logging
 
-URL_LOG = "https://lms.kgeu.ru/login/index.php"
-URL_MAIN = "https://lms.kgeu.ru/course/view.php?id=588&section=3"
-DATA = {
-    "fmannanov01@gmail.com": "p57aw63Hy0",
-    "lol-lol12@inbox.ru": "P67NZ43pL0",
-    "danis2405@icloud.com": "M65Ft70za3",
-    "sadwoxy@mail.ru": "t53JF70LK4"
-}
+import telegram as tg
+from selenium import webdriver
+from dotenv import load_dotenv
+from logging.handlers import RotatingFileHandler
+
+
+load_dotenv()
+
+CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+DATA = ast.literal_eval(os.getenv('DATA'))
+URL_LOG = os.getenv('URL_LOG')
+URL_MAIN = os.getenv('URL_MAIN')
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s, %(name)s, %(levelname)s, %(message)s'
+)
+logger = logging.getLogger(__name__)
+logger.setLevel('DEBUG')
+handler = RotatingFileHandler('mylog.log', maxBytes=50000000, backupCount=1)
+logger.addHandler(handler)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+handler.setFormatter(formatter)
+
+bot = tg.Bot(token=BOT_TOKEN)
+logger.debug('Bot is ready')
+
+def send_message(message): 
+    return bot.send_message(chat_id=CHAT_ID, text=message) 
+
 def main(login, password):
-    driver = webdriver.Chrome()
-    driver.get(URL_LOG)
-    username_input = driver.find_element_by_id("username")
-    username_input.clear()
-    username_input.send_keys(login)
-    password_input = driver.find_element_by_id("password")
-    password_input.clear()
-    password_input.send_keys(password)
-    driver.find_element_by_id("loginbtn").click()
-    driver.get(URL_MAIN)
-    driver.close()
-    driver.quit()
+    try:
+        driver = webdriver.Chrome()
+        driver.get(URL_LOG)
+        username_input = driver.find_element_by_id("username")
+        username_input.clear()
+        username_input.send_keys(login)
+        password_input = driver.find_element_by_id("password")
+        password_input.clear()
+        password_input.send_keys(password)
+        driver.find_element_by_id("loginbtn").click()
+        driver.get(URL_MAIN)
+        driver.close()
+        driver.quit()
+    except Exception as e:
+        send_message(f'Бот упал с ошибкой: {e}')
+        logger.error(e, exc_info=True)
     
 
 if __name__ == '__main__':
     while True:
-        for login, password in DATA.items():
-            main(login, password)
-            print(f'Произошёл вход с аккаунта {login}')
-        time.sleep(172800)
+        try:
+            for login, password in DATA.items():
+                main(login, password)
+                send_message(f'Произошёл вход с аккаунта {login}')
+            for i in range (1,144):
+                requests.get('https://www.google.com/')
+                time.sleep(20 * 60)
+        except Exception as e:
+            send_message(f'Бот упал с ошибкой: {e}')
+            logger.error(e, exc_info=True)
+            time.sleep(60)
